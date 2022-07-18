de91291d309650f750e877aa0dcc53ba1777e7be
tgoandrex <tgoandrex@gmail.com>
2022-07-17 21:14:03 -0500

    Test

--- webpack.common.js
+++ webpack.common.js
@@ -1,20 +1,23 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+require('dotenv').config({ path: './.env' });
 
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: './src/index.js',
     plugins : [
         new HtmlWebpackPlugin({
-            template: "./src/template.html"
+            template: './src/template.html'
         }),
-        new Dotenv(),
+        new webpack.DefinePlugin({
+            'process.env': JSON.stringify(process.env)
+        })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+                use: ['style-loader', 'css-loader']
             }
         ]
     }
